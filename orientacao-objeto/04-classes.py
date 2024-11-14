ebac0c85138e6bf468f82abda099441a36753d8e
lucasfmarques2 <lucasfreitasm3@hotmail.com>
2024-11-14 17:45:15 -0300

    Adicionando estadi stop

--- orientacao-objeto/04-classes.py
+++ orientacao-objeto/04-classes.py
@@ -12,12 +12,26 @@ class Camera:
         print(f'{self.nome} está filmando')
         self.filmando = True
         
+    def stop(self):
+        if not self.filmando:
+            print(f'{self.nome} não está filmando')
+            return
+        print(f'{self.nome} está parando de filmar...')
+        self.filmando = False
 
+    def fotografar(self):
+        if self.filmando:
+            print(f'{self.nome} não pode fotografar enquanto filma')
+            return
+        print(f'{self.nome} foto tirada')
 
 c1 = Camera('Cannon')
 c2 = Camera('tequepize')
 
 c1.filmar()
-c1.filmar()
+c1.stop()
+c1.fotografar()
+c1.stop()
+
 
         
